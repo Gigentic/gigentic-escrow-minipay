581f5e3dbcf3f99c931583f1495d684482aeb3bc
Celo Composer <support@celo.org>
2025-10-12 21:00:38 +0100

    hardhat deployment works

--- apps/contracts/hardhat.config.ts
+++ apps/contracts/hardhat.config.ts
@@ -1,6 +1,6 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
-// import "dotenv/config";
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
   solidity: {
