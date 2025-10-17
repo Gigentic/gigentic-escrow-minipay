51f1538e22c80b59f289c90948a07b641cc98d31
Celo Composer <support@celo.org>
2025-10-17 16:45:04 +0100

    escrow frontend with hydration error

---
+++ apps/web/src/lib/escrow-config.ts
@@ -0,0 +1,136 @@
+import { Address } from "viem";
+
+// SimpleEscrow contract ABI
+export const SIMPLE_ESCROW_ABI = [
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_recipient",
+        type: "address",
+      },
+    ],
+    stateMutability: "payable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "amount",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "depositor",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "escrowInfo",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "isDeposited",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "isReleased",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "recipient",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "release",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+] as const;
+
+// Contract address from environment variable
+export const ESCROW_CONTRACT_ADDRESS =
+  (process.env.NEXT_PUBLIC_ESCROW_CONTRACT_ADDRESS as Address) ||
+  "0x5549E67B9EEf5963c84BafEA64DD81bd5C72947c";
+
+// Type for escrow info
+export interface EscrowInfo {
+  depositor: Address;
+  recipient: Address;
+  amount: bigint;
+  isDeposited: boolean;
+  isReleased: boolean;
+}
+
